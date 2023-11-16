62c87a010e64af150f9404916c5272c6838f2d71
Lucas SOKOL <lusokol@made-f0Ar1s1.clusters.42paris.fr>
2023-11-16 18:20:45 +0100

    ex00 done

---
+++ day00/ex00/Hello.py
@@ -0,0 +1,31 @@
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
+
+#list change
+ft_list[1] = 'World!'
+
+#tuple change
+tuple_tmp = list(ft_tuple)
+tuple_tmp[1] = 'France!'
+ft_tuple = tuple_tmp
+
+#set change
+ft_set.clear()
+ft_set.add('Paris!')
+ft_set.add('Hello')
+ft_set = sorted(ft_set)
+
+# doesn't work everytime???
+#ft_set = {"Hello", "Paris!"}
+
+#ft_set = sorted(ft_set.symmetric_difference({'tutu!', 'Paris!'}))
+
+#dict change
+ft_dict.update(Hello='42Paris!')
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
