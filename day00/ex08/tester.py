a1c0d852e13337dbcb67acc4374f72b57a88c6ee
Lucas sokol <sokol.lucas@gmail.com>
2024-03-04 12:20:31 +0100

    merge

--- day00/ex08/tester.py
+++ day00/ex08/tester.py
@@ -1,9 +1,10 @@
 from time import sleep
 from tqdm import tqdm
-#from Loading import ft_tqdm
-#for elem in ft_tqdm(range(333)):
-#sleep(0.005)
-#print()
-for elem in tqdm(range(333)):
-	sleep(0.005)
+from Loading import ft_tqdm
+x = 10000
+for elem in ft_tqdm(range(x)):
+	sleep(0.0005)
+print()
+for elem in tqdm(range(x)):
+	sleep(0.0005)
 print()
